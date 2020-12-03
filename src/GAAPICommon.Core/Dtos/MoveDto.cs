4b1d4f5883af83e6dd0e460e27f1b6d9364a8728
Martin Davies <martin.davies@guidanceautomation.com>
2020-12-03 17:29:11 +0000

    Adds Dtos from Scheduling.Management
    Adds scheduling management dtos

---
+++ src/GAAPICommon.Core/Dtos/MoveDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace GAAPICommon.Core.Dtos
+{
+    public class MoveDto
+    {
+        [DataMember]
+        public string Alias { get; set; }
+
+        [DataMember]
+        public int DestinationId { get; set; }
+
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public int SourceId { get; set; }
+    }
+}
