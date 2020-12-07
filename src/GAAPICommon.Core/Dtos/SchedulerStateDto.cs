d9d34c8799830290030f8233d64b0e7ccfa2d76f
Martin Davies <martin.davies@guidanceautomation.com>
2020-12-07 13:54:32 +0000

    Create SchedulerStateDto.cs

---
+++ src/GAAPICommon.Core/Dtos/SchedulerStateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GAAPICommon.Core.Dtos
+{
+    public class SchedulerStateDto
+    {
+        bool IsInAttractMode { get; set; }
+
+        bool IsSpotManagementEnabled { get; set; }
+
+        bool IsUpdateLoopPluginsEnabled { get; set; }
+    }
+}
