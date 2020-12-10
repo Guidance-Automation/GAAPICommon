662182e62cf6f47f9ece75a444f3b51b3f8e6cff
Martin Davies <martin.davies@guidanceautomation.com>
2020-12-10 14:12:55 +0000

    Create AgentDto.cs
    Adds agent dto

---
+++ src/GAAPICommon.Core/Dtos/AgentDto.cs
@@ -0,0 +1,22 @@
+﻿using GAAPICommon.Architecture;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace GAAPICommon.Core.Dtos
+{
+    [DataContract]
+    public class AgentDto
+    {
+        [DataMember]
+        public AgentLifetimeState AgentLifetimeState { get; set; }
+
+        [DataMember]
+        public string Alias { get; set; }
+
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public IPAddress IPAddress { get; set; }
+    }
+}
