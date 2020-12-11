31eac840e2f5dfdbda8f022e5737d65225b72ddc
Martin Davies <martin.davies@guidanceautomation.com>
2020-12-11 14:03:32 +0000

    Adding Dtos from AMR management service

---
+++ src/GAAPICommon.Core/Dtos/VirtualBatteryProfileModelDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GAAPICommon.Core.Dtos
+{
+    public class VirtualBatteryProfileModelDto
+    {
+        public double HighDischargeRate { get; set; }
+
+        public double LowDischargeRate { get; set; }
+
+        public double ChargeRate { get; set; }
+    }
+}
