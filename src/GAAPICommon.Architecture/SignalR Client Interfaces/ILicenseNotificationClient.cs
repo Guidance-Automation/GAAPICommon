bf08fd8e4e9bf599f59ef7e5783706952c69fa5f
CD-Clarke <36769328+CD-Clarke@users.noreply.github.com>
2020-12-14 12:06:59 +0000

    Fix namespace issue

--- src/GAAPICommon.Architecture/SignalR Client Interfaces/ILicenseNotificationClient.cs
+++ src/GAAPICommon.Architecture/SignalR Client Interfaces/ILicenseNotificationClient.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GAAPICommon.Architecture.SignalR_Client_Interfaces
+namespace GAAPICommon.Architecture
 {
 	public interface ILicenseNotificationClient
 	{
