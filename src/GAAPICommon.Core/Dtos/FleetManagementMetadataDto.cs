11817f473b1c86b5d0ace61dad580cd1ef2594d2
Martin Davies <martin.davies@guidanceautomation.com>
2021-01-14 17:19:46 +0000

    Adds fleet management metadata dto

---
+++ src/GAAPICommon.Core/Dtos/FleetManagementMetadataDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GAAPICommon.Core.Dtos
+{
+    public class FleetManagementMetadataDto
+    {
+        public string ProductName { get; set; }
+
+        public SemVerDto SemVer { get; set; }
+    }
+}
