d12e0fd8f62414c9c91b5ed58aa09f83cfabecce
mbrewer32 <mbrewer32@hotmail.com>
2021-02-09 09:11:25 -0800

    Create ModuleInfoDto.cs
    Created DTO for module information, containing only display name for now.

---
+++ src/GAAPICommon.Core/Dtos/ModuleInfoDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.Serialization;
+
+namespace GAAPICommon.Core.Dtos
+{    
+    public class ModuleInfoDto
+    {
+        [DataMember]
+        public string DisplayName { get; set; } = string.Empty;
+    }
+}
