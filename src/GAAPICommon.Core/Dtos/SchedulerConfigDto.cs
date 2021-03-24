587e4cb7d9aac14f8d20083071d8386957ccdf82
Martin Davies <martin.davies@guidanceautomation.com>
2021-03-24 16:23:47 +0000

    Adds Spot Manager State Dto
    AI-845 exposes the state of the spot manager.

--- src/GAAPICommon.Core/Dtos/SchedulerConfigDto.cs
+++ src/GAAPICommon.Core/Dtos/SchedulerConfigDto.cs
@@ -1,5 +1,8 @@
 ﻿namespace GAAPICommon.Core.Dtos
 {
+    /// <summary>
+    /// Includes the spot manager config as a sub-item
+    /// </summary>
     public class SchedulerConfigDto
     {
         public bool IsInAttractMode { get; set; } = false;
@@ -9,5 +12,7 @@
         public bool IsUpdateLoopPluginsEnabled { get; set; } = false;
 
         public MapMetadataDto MapMetadata { get; set; } = null;
+
+        public SpotManagerConfigDto SpotManagerConfig { get; set; } = null;
     }
 }
--- src/GAAPICommon.Core/Dtos/SchedulerStateDto.cs
+++ src/GAAPICommon.Core/Dtos/SchedulerStateDto.cs
@@ -2,6 +2,9 @@
 
 namespace GAAPICommon.Core.Dtos
 {
+    /// <summary>
+    /// Includes the spot manager state as a sub-item
+    /// </summary>
     public class SchedulerStateDto
     {
         public Guid InstanceId { get; set; }
