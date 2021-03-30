502e5e53965b73816e1e9a9f2aa2a16625ca86bb
mbrewer32 <mbrewer32@hotmail.com>
2021-03-29 18:01:25 -0700

    Enable .NET Core Fleet State clients
    Provide a dedicated interface and DTOs so that .NET Core clients can receive Fleet State update events unimpeded.

---
+++ src/GAAPICommon.Architecture/IKingpinStateCore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GAAPICommon.Architecture
+{
+    public interface IKingpinStateCore : IKingpinStatusReporter
+    {
+        string Alias { get; }
+
+        bool IsVirtual { get; }
+
+        byte Tick { get; }
+
+        float X { get; }
+
+        float Y { get; }
+
+        float Heading { get; }
+
+        MovementType CurrentMovementType { get; }
+
+        long IPAddress { get; }
+
+        byte[] StateCastExtendedData { get; }
+
+        double Speed { get; }
+
+        int WaypointLastId { get; }
+
+        int WaypointNextId { get; }
+
+        AgvMode AgvMode { get; }
+
+        double BatteryChargePercentage { get; }
+
+        ExtendedDataFaultStatus ExtendedDataFaultStatus { get; }
+
+        FrozenState FrozenState { get; }
+
+        bool IsCharging { get; }
+
+        int LastCompletedInstructionId { get; }
+
+        TimeSpan Stationary { get; }
+
+        byte[] CurrentWaypointExtendedData { get; }
+    }
+}
