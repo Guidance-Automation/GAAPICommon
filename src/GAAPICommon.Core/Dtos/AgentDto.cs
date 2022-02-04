ccac2238df68b7af7ddaac7b130e3f7244921028
Christopher Traina <christopher.traina@raf.com>
2022-02-04 12:56:19 -0800

    Update AgentDto.cs

--- src/GAAPICommon.Core/Dtos/AgentDto.cs
+++ src/GAAPICommon.Core/Dtos/AgentDto.cs
@@ -5,40 +5,41 @@ using System.Runtime.Serialization;
 
 namespace GAAPICommon.Core.Dtos
 {
-	public enum AgentMarkovState
-	{
-		[EnumMember]
-		Moving = 2,
+    [DataContract]
+    public enum AgentMarkovState
+    {
+	[EnumMember]
+	Moving = 2,
 
-		[EnumMember]
-		Error = 5,
+	[EnumMember]
+	Error = 5,
 
-		[EnumMember]
-		Fatal = 6,
+	[EnumMember]
+	Fatal = 6,
 
-		[EnumMember]
-		Servicing = 4,
+	[EnumMember]
+	Servicing = 4,
 
-		[EnumMember]
-		Ready = 1,
+	[EnumMember]
+	Ready = 1,
 
-		[EnumMember]
-		Sleeping = 3,
+	[EnumMember]
+	Sleeping = 3,
 
-		[EnumMember]
-		Waiting = 0,
+	[EnumMember]
+	Waiting = 0,
 
-		[EnumMember]
-		Stationary = 7,
+	[EnumMember]
+	Stationary = 7,
 
-		[EnumMember]
-		Executing = 8,
+	[EnumMember]
+	Executing = 8,
 
-		[EnumMember]
-		Aborting = 9
-	};
-	
-	[DataContract]
+	[EnumMember]
+	Aborting = 9
+    };
+
+    [DataContract]
     public class AgentDto
     {
         [DataMember]
@@ -54,13 +55,13 @@ namespace GAAPICommon.Core.Dtos
         [JsonConverter(typeof(IPAddressJsonConverter))]
         public IPAddress IPAddress { get; set; }
 
-		[DataMember]
-		public AgentMarkovState MarkovState { get; set; }
+	[DataMember]
+	public AgentMarkovState MarkovState { get; set; }
 
-		[DataMember]
-		public int CurrentJobId { get; set; }
+	[DataMember]
+	public int CurrentJobId { get; set; }
 
-		[DataMember]
-		public int CurrentNodeId { get; set; }
+	[DataMember]
+	public int CurrentNodeId { get; set; }
     }
 }
