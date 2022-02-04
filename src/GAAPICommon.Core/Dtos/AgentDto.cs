558ba1b84cedc7e8638aeda6bbba4f13f60225d2
Christopher Traina <christopher.traina@raf.com>
2022-02-04 11:39:02 -0800

    Update AgentDto.cs

--- src/GAAPICommon.Core/Dtos/AgentDto.cs
+++ src/GAAPICommon.Core/Dtos/AgentDto.cs
@@ -5,7 +5,40 @@ using System.Runtime.Serialization;
 
 namespace GAAPICommon.Core.Dtos
 {
-    [DataContract]
+	public enum AgentMarkovState
+	{
+		[EnumMember]
+		Moving = 2,
+
+		[EnumMember]
+		Error = 5,
+
+		[EnumMember]
+		Fatal = 6,
+
+		[EnumMember]
+		Servicing = 4,
+
+		[EnumMember]
+		Ready = 1,
+
+		[EnumMember]
+		Sleeping = 3,
+
+		[EnumMember]
+		Waiting = 0,
+
+		[EnumMember]
+		Stationary = 7,
+
+		[EnumMember]
+		Executing = 8,
+
+		[EnumMember]
+		Aborting = 9
+	};
+	
+	[DataContract]
     public class AgentDto
     {
         [DataMember]
@@ -20,5 +53,14 @@ namespace GAAPICommon.Core.Dtos
         [DataMember]
         [JsonConverter(typeof(IPAddressJsonConverter))]
         public IPAddress IPAddress { get; set; }
+
+		[DataMember]
+		public AgentMarkovState MarkovState { get; set; }
+
+		[DataMember]
+		public int CurrentJobId { get; set; }
+
+		[DataMember]
+		public int CurrentNodeId { get; set; }
     }
 }
