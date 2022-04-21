5c16e2c913e1b70b0cb6b171945dffb4b606e4f3
Nick Strathy <nstrathy@jmihome.com>
2022-04-20 18:20:47 -0700

    AI-1096 add IsChanged property to ISpotManagerState

--- src/GAAPICommon.Core/Dtos/SpotManagerStateDto.cs
+++ src/GAAPICommon.Core/Dtos/SpotManagerStateDto.cs
@@ -12,11 +12,14 @@ namespace GAAPICommon.Core.Dtos
         public IEnumerable<ChargingSpotStateDto> ChargingSpotStateDtos { get; set; } = Enumerable.Empty<ChargingSpotStateDto>();
 
         [DataMember]
-        public IEnumerable<ParkingSpotStateDto> ParkingSpotStateDtos { get; set;  } = Enumerable.Empty<ParkingSpotStateDto>();
+        public IEnumerable<ParkingSpotStateDto> ParkingSpotStateDtos { get; set; } = Enumerable.Empty<ParkingSpotStateDto>();
 
         public IEnumerable<IChargingSpotState> ChargingSpotStates => ChargingSpotStateDtos;
 
         public IEnumerable<IParkingSpotState> ParkingSpotStates => ParkingSpotStateDtos;
+
+        [DataMember]
+        public bool IsChanged { get; set; } = false;
 
         [DataMember]
         public byte Tick { get; set; }
